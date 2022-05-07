0a9c0e0666aa4cf0bf305406f9f2510a143660a0
Shruti Gawas <shruti.gawas@anant.co.in>
2022-05-07 16:07:00 +0100

    first commit

---
+++ SwiftUIAnimations/Shapes/RocketShape.swift
@@ -0,0 +1,20 @@
+//
+//  RocketShape.swift
+//  SwiftUIAnimations
+//
+//  Created by Vineal Viji Varghese on 07/05/2022.
+//
+
+import SwiftUI
+
+struct RocketShape: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RocketShape_Previews: PreviewProvider {
+    static var previews: some View {
+        RocketShape()
+    }
+}
